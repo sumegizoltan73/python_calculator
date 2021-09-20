ad12b3bec2f8081c32b01f8f019a4f6a04d34676
Sümegi Zoltán <sumegizoltan73@gmail.com>
2021-09-20 14:09:47 +0200

    first commit

---
+++ Calculator.py
@@ -0,0 +1,38 @@
+
+import os
+os.environ['KIVY_NO_CONSOLELOG'] = '0'
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+
+Builder.load_file("kv-files/Calculator.kv")
+
+class CalculatorLayout(BoxLayout):
+
+    clear = False
+
+    def calculate(self):
+        expression = self.ids["display"].text
+
+        try:
+            exp = str(eval(expression))
+        except:
+            exp = "Invalid operation!"
+            self.clear = True
+
+        self.ids["display"].text = exp
+
+    def button_pressed(self, btn):
+        if self.clear:
+            self.ids["display"].text = ""
+            self.clear = False
+
+        text = btn.text
+        self.ids["display"].text += text
+
+class CalculatorApp(App):
+    def build(self):
+        return CalculatorLayout()
+
+CalculatorApp().run()
